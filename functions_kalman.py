10951301bec4a871d47b61fd49f8b428e4f177da
Samuel Monnier <samuel.monnier@gmail.com>
2019-04-03 13:08:17 +0200

    MAINT PEP8

--- functions_kalman.py
+++ functions_kalman.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from pyfinance.ols import OLS, RollingOLS, PandasRollingOLS
-import pykalman
 from pykalman import KalmanFilter
 from scipy.interpolate import splrep, splev
 from scipy.stats import wilcoxon, ttest_rel
@@ -19,15 +18,14 @@ colors = prop_cycle.by_key()['color']
 # Preparation
 # -----------
 
-# Sensitivities
+# Sensitivities / factor loadings
 def get_sensitivities(timespan, sens_type):
     pi = 3.1415
     if sens_type == "Various smooth":
-        t = np.linspace(0,timespan-1,timespan,dtype=np.float64)
+        t = np.linspace(0, timespan-1, timespan, dtype=np.float64)
         sens1 = -(pd.Series(np.arctan((t-timespan/2)/timespan*100)) - pi/2)/pi
         sens2 = (pd.Series(np.sin(t/timespan*3.2 - 1.5)) + 1)/2
-        sens3 = pd.Series(np.sin(t/timespan*5) + np.sin(t/timespan*10 + 150) + \
-                          np.sin(t/timespan*20 + 150))/2
+        sens3 = pd.Series(np.sin(t/timespan*5) + np.sin(t/timespan*10 + 150) + np.sin(t/timespan*20 + 150))/2
     elif sens_type == "Random walk":
         sens1 = pd.Series(np.random.normal(size=timespan)).cumsum()/200 + 1
         sens2 = pd.Series(np.random.normal(size=timespan)).cumsum()/200
@@ -49,7 +47,6 @@ def create_random_sensitivity(timespan):
     x.sort()
     y = 2*np.random.rand(num_points + 2) - 1
 
-    
     spleen = splrep(x, y, s=smoothing_parameter)
     t = np.linspace(0,timespan-1,timespan,dtype=np.float64)
     values = splev(t, spleen)
@@ -79,7 +76,7 @@ def get_returns(timespan, factors, sens, error_std):
 def estimate_sensitivities_cr(factors, returns):
     reg = linear_model.LinearRegression()
     reg.fit(factors, returns)
-    return pd.DataFrame(data = [reg.coef_], index = factors.index)
+    return pd.DataFrame(data=[reg.coef_], index=factors.index)
     
     
 def estimate_sensitivities_rr(factors, returns, window):
@@ -89,7 +86,7 @@ def estimate_sensitivities_rr(factors, returns, window):
     
 def estimate_sensitivities_err(factors, returns, lambd):
     num_factors = factors.shape[1]
-    ff = np.zeros([num_factors,num_factors])
+    ff = np.zeros([num_factors, num_factors])
     fr = np.zeros(num_factors)
     est_sens_list = []
     for i, row in factors.iterrows():
@@ -105,14 +102,14 @@ def estimate_sensitivities_kf(factors, returns, covariance_ratio):
     n_dim_obs=1
     observation_covariance = 1
     transition_covariance = np.eye(n_dim_state)*covariance_ratio
-    observation_matrices = factors.values[:,np.newaxis,:]
+    observation_matrices = factors.values[:, np.newaxis, :]
     kf = KalmanFilter(initial_state_mean=np.zeros(n_dim_state), 
                       n_dim_state=n_dim_state, 
                       n_dim_obs=n_dim_obs, 
                       observation_matrices=observation_matrices,
                       observation_covariance=observation_covariance,
                       transition_covariance=transition_covariance
-                     )
+                      )
     filtered_state_means, filtered_state_covariances = kf.filter(returns)
     return pd.DataFrame(filtered_state_means)
     
@@ -127,10 +124,8 @@ def estimate_sensitivities_stkf(factors, returns, covariance_ratio):
     n_dim_obs=1
     observation_covariance = 1
     transition_covariance = covariance_ratio*np.eye(n_dim_state)
-    observation_matrices = np.array([((i+1)%2)*factors.values[:, math.floor(i/2)] for i in range(n_dim_state)]).transpose()[:, np.newaxis,:]
-    """observation_matrices = np.array([factors.values[:, 0], 0*factors.values[:, 0],
-                                     factors.values[:, 1], 0*factors.values[:, 1],
-                                     factors.values[:, 2], 0*factors.values[:, 2]]).transpose()[:, np.newaxis,:]"""
+    observation_matrices = np.array([((i+1)%2)*factors.values[:, math.floor(i/2)] for i in range(n_dim_state)
+                                     ]).transpose()[:, np.newaxis, :]
     transition_matrix = np.kron(np.eye(factors.shape[1]), np.array([[1, 1], [0, 1]]))
     kf = KalmanFilter(initial_state_mean=np.zeros(n_dim_state), 
                       n_dim_state=n_dim_state, 
@@ -141,7 +136,7 @@ def estimate_sensitivities_stkf(factors, returns, covariance_ratio):
                       transition_matrices=transition_matrix
                      )
     filtered_state_means, filtered_state_covariances = kf.filter(returns)
-    return pd.DataFrame(filtered_state_means[:,[i for i in range(n_dim_state) if i%2 == 0]])
+    return pd.DataFrame(filtered_state_means[:, [i for i in range(n_dim_state) if i % 2 == 0]])
     
 
 # Evaluation
@@ -150,14 +145,14 @@ def estimate_sensitivities_stkf(factors, returns, covariance_ratio):
 def plot_estimated_sensitivities(sens, est_sens, colors):
     fig, ax = plt.subplots(2, 2, figsize=(12, 8))
     axc = ax[0,0]
-    axc.plot(est_sens.iloc[:,0], c=colors[0])
-    axc.plot(sens.iloc[:,0], '--', c='k')
-    axc = ax[0,1]
-    axc.plot(est_sens.iloc[:,1], c=colors[1])
-    axc.plot(sens.iloc[:,1], '--', c='k')
-    axc = ax[1,0]
-    axc.plot(est_sens.iloc[:,2], c=colors[2])
-    axc.plot(sens.iloc[:,2], '--', c='k')
+    axc.plot(est_sens.iloc[:, 0], c=colors[0])
+    axc.plot(sens.iloc[:, 0], '--', c='k')
+    axc = ax[0, 1]
+    axc.plot(est_sens.iloc[:, 1], c=colors[1])
+    axc.plot(sens.iloc[:, 1], '--', c='k')
+    axc = ax[1, 0]
+    axc.plot(est_sens.iloc[:, 2], c=colors[2])
+    axc.plot(sens.iloc[:, 2], '--', c='k')
   
   
 def compute_estimated_returns(factors, est_sens):
@@ -186,30 +181,27 @@ def print_weighted_accuracy_performance(estimated_returns, returns, name, perfor
         print(f"Weighted accuracy for {name}, assuming the factors known: {weighted_accuracy}")
    
    
-def plot_prediction_performance(factors, returns, est_sens, name, 
-        performance_record_mse, performance_record_wacc):
+def plot_prediction_performance(factors, returns, est_sens, name, performance_record_mse, performance_record_wacc):
     estimated_returns = compute_estimated_returns(factors, est_sens)
     print_mse_performance(estimated_returns, returns, name, performance_record_mse)
     print_weighted_accuracy_performance(estimated_returns, returns, name, performance_record_wacc)
     plot_residuals(estimated_returns, returns)
   
 
-def estimate_regression_performance(factors, returns, sensitivities, name,
-                                    performance_record_mse, performance_record_wacc, 
-                                    **kwargs):
+def estimate_regression_performance(factors, returns, sensitivities, name, performance_record_mse,
+                                    performance_record_wacc, **kwargs):
     start_time = 1000
     if name[:12] == "Constant OLS":
         estimated_sensitivities = estimate_sensitivities_cr(factors, returns)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time:, :]
     elif name[:11] == "Rolling OLS":
         rolling_ols_window = kwargs['rolling_ols_window']
         estimated_sensitivities = estimate_sensitivities_rr(factors, returns, rolling_ols_window)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time - rolling_ols_window + 1:,:]
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time - rolling_ols_window + 1:, :]
     elif name[:23] == "Exponential rolling OLS":
         exp_rolling_ols_lambda = kwargs['exp_rolling_ols_lambda']
-        estimated_sensitivities = estimate_sensitivities_err(factors, returns, 
-                                                             exp_rolling_ols_lambda)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = estimate_sensitivities_err(factors, returns, exp_rolling_ols_lambda)
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time:, :]
     elif name[:18] == "Kalman naive trend":
         kalman_covariance_ratio = kwargs['kalman_covariance_ratio']
         nt_factor = kwargs['nt_factor']
@@ -218,20 +210,20 @@ def estimate_regression_performance(factors, returns, sensitivities, name,
                                                                kalman_covariance_ratio)
         estimated_sensitivities = estimate_sensitivities_ntc(estimated_sensitivities_kf, 
                                                              nt_factor, nt_window)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time:, :]
     elif name[:18] == "Kalman local trend":
         stkf_covariance_ratio = kwargs['stkf_covariance_ratio']
         estimated_sensitivities = estimate_sensitivities_stkf(factors, returns, stkf_covariance_ratio)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time:, :]
     elif name[:6] == "Kalman":
         kalman_covariance_ratio = kwargs['kalman_covariance_ratio']
         estimated_sensitivities = estimate_sensitivities_kf(factors, returns, kalman_covariance_ratio)
-        estimated_sensitivities = estimated_sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = estimated_sensitivities.iloc[start_time:, :]
     elif name == "Oracle":
-        estimated_sensitivities = sensitivities.iloc[start_time:,:]
+        estimated_sensitivities = sensitivities.iloc[start_time:, :]
     else:
         print("Unknown name")
-    estimated_returns = compute_estimated_returns(factors.iloc[start_time:,:], estimated_sensitivities)
+    estimated_returns = compute_estimated_returns(factors.iloc[start_time:, :], estimated_sensitivities)
     print_mse_performance(estimated_returns, 
                           returns.iloc[start_time:], 
                           name, 
@@ -242,6 +234,7 @@ def estimate_regression_performance(factors, returns, sensitivities, name,
                                         name, 
                                         performance_record_wacc,
                                         verbose=False) 
+
 
 def compare_hyperparameters_mp(algo_name, num_samples, timespan, sens_type, return_noise, **kwargs):
     """
@@ -272,6 +265,7 @@ def compare_hyperparameters_mp(algo_name, num_samples, timespan, sens_type, retu
             list_performance_record_wacc.append(out[2])
     
     return pd.DataFrame(list_performance_record_mse), pd.DataFrame(list_performance_record_wacc)
+
 
 def generate_sample_hyperparameters(sens_type, timespan, return_noise, algo_name, all_values, i):
     print(i)
@@ -334,7 +328,7 @@ def generate_sample_performance(sens_type, timespan, return_noise, algo_paramete
     estimate_regression_performance(factors, returns, sens, "Kalman", 
                                     performance_record_mse, performance_record_wacc, 
                                     kalman_covariance_ratio=kalman_covariance_ratio)
-    #estimate_regression_performance(factors, returns, "Kalman naive trend", 
+    # estimate_regression_performance(factors, returns, "Kalman naive trend",
     #                                performance_record_mse, performance_record_wacc, 
     #                                kalman_covariance_ratio=kalman_covariance_ratio, 
     #                                nt_factor=nt_factor, nt_window=nt_window)
@@ -346,19 +340,25 @@ def generate_sample_performance(sens_type, timespan, return_noise, algo_paramete
                                     stkf_covariance_ratio=stkf_covariance_ratio)
     return i, performance_record_mse, performance_record_wacc
     
-    
+
+def wil_pvalue(x):
+    return wilcoxon(x)[1]
+
+
+def t_pvalue(x):
+    return ttest_rel(x, np.zeros(len(performance)))[1]
+
+
 def performance_summary(performance, metric):
     mean_metric = performance.mean()
     if metric == "Weighted accuracy":
         ordered_metric = mean_metric.sort_values(ascending=False)
     elif metric == "MSE":
         ordered_metric = mean_metric.sort_values()
-    
     relative_performance = performance[ordered_metric.index].diff(-1, axis=1)
-    wil_pvalue = lambda x : wilcoxon(x)[1]
+
     w_test = relative_performance.apply(wil_pvalue, axis=0)
     w_test[-1] = np.nan
-    t_pvalue = lambda x : ttest_rel(x, np.zeros(len(performance)))[1]
     t_test = relative_performance.apply(t_pvalue, axis=0)
 
     summary = pd.concat([ordered_metric, t_test, w_test], axis=1)
@@ -367,42 +367,42 @@ def performance_summary(performance, metric):
     summary.to_latex("summary.tex")
     return summary
     
-    
-    
+
 def compute_t_stat(performance, reference=None):
     if reference is None:
-        ref_column = performance.iloc[:,0]
+        ref_column = performance.iloc[:, 0]
     else:
-        ref_column = performance.loc[:,reference]
-    rel_performance = performance.subtract(ref_column, axis = 0)
+        ref_column = performance.loc[:, reference]
+    rel_performance = performance.subtract(ref_column, axis=0)
     t_stat = rel_performance.mean().divide(rel_performance.std()/np.sqrt(len(rel_performance)))
     if reference is None:
         t_stat.iloc[0] = 0
     else:
         t_stat.loc[reference] = 0
     return t_stat
-    
+
+
 def compute_wilcoxon_pvalue(performance, reference=None):
     if reference is None:
-        ref_column = performance.iloc[:,0]
+        ref_column = performance.iloc[:, 0]
     else:
-        ref_column = performance.loc[:,reference]
+        ref_column = performance.loc[:, reference]
     rel_performance = performance.subtract(ref_column, axis = 0)
-    wil_pvalue = lambda x : wilcoxon(x)[1]
     w_test = rel_performance.apply(wil_pvalue, axis=0)
     if reference is None:
         w_test.iloc[0] = np.nan
     else:
         w_test.loc[reference] = np.nan
     return w_test
-    
+
+
 def plot_perf_differences(performance, reference=None):
     if reference is None:
-        ref_column = performance.iloc[:,0]
-        purged_performance = performance.iloc[:,1:]
+        ref_column = performance.iloc[:, 0]
+        purged_performance = performance.iloc[:, 1:]
     else:
-        ref_column = performance.loc[:,reference]
+        ref_column = performance.loc[:, reference]
         purged_performance = performance.drop([reference], axis=1)
-    rel_performance = purged_performance.subtract(ref_column, axis = 0)
-    rel_performance.hist(layout=(purged_performance.shape[1], 1), bins=200, sharex=True, sharey=True, figsize=(12,8))
+    rel_performance = purged_performance.subtract(ref_column, axis=0)
+    rel_performance.hist(layout=(purged_performance.shape[1], 1), bins=200, sharex=True, sharey=True, figsize=(12, 8))
     plt.tight_layout()
